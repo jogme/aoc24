0e74df1379ec124488244310c97e9e85b7328758
Norbert Pocs <norbertpocs0@gmail.com>
2025-03-10 08:39:00 +0100

    cpp: Add skeleton for day one
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

---
+++ cpp/one.cpp
@@ -0,0 +1,37 @@
+/* File to solve the first task of aoc 24
+ * @author: Norbert Pocs
+ */
+
+#include <iostream>
+
+#include "Parser.h"
+
+/* Parser should already be loaded here */
+static int solve_a(Parser& p) {
+    (void)p;
+    return 0;
+}
+
+/* Parser should already be loaded here */
+static int solve_b(Parser& p) {
+    (void)p;
+    return 0;
+}
+
+void solve_one() {
+    Parser p(1, true);
+    p.tokenize_data();
+    int a, b;
+
+    a = solve_a(p);
+    b = solve_b(p);
+
+    std::cout << "a: " << a << "\n";
+    std::cout << "b: " << b << "\n";
+}
+
+int main() {
+    solve_one();
+
+    return 0;
+}
