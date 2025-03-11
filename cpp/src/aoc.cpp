4d40d80b07ee1bb5ee2da0ff0e2c078007e792cf
Norbert Pocs <norbertpocs0@gmail.com>
2025-03-11 15:49:29 +0100

    cpp: solve puzzle day one
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

--- cpp/src/aoc.cpp
+++ cpp/src/aoc.cpp
@@ -1,3 +1,13 @@
+/* The main file of the program.
+ * @author: Norbert Pocs
+ */
+
+#include "Parser.h"
+#include "aoc.h"
+
 int main() {
+    Parser p(1, false);
+
+    solve_one(p);
     return 0;
 }
--- cpp/src/one.cpp
+++ cpp/src/one.cpp
@@ -3,27 +3,51 @@
  */
 
 #include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <numeric>
+#include <map>
 
 #include "Parser.h"
 
 /* Parser should already be loaded here */
-static int solve_a(Parser& p) {
-    (void)p;
-    return 0;
+static int solve_a(std::vector <int> vi1, std::vector <int> vi2) {
+    std::vector <int> tmp(vi1.size());
+    std::transform(vi1.begin(), vi1.end(), vi2.begin(), tmp.begin(), [](int a, int b){return std::abs(a-b);});
+
+    return std::reduce(tmp.begin(), tmp.end());
 }
 
 /* Parser should already be loaded here */
-static int solve_b(Parser& p) {
-    (void)p;
-    return 0;
+static int solve_b(std::vector <int> vi1, std::vector <int> vi2) {
+    std::map <int, int> occurrence;
+    std::vector <int> tmp;
+
+    for (const auto& i : vi1) {
+        int o = std::count(vi2.begin(), vi2.end(), i);
+        occurrence[i] = o;
+    }
+    for (const auto& i : vi1) {
+        tmp.push_back(i*occurrence[i]);
+    }
+    return std::reduce(tmp.begin(), tmp.end());
 }
 
 void solve_one(Parser& p) {
     p.tokenize_data();
     int a, b;
+    std::vector <int> vi1;
+    std::vector <int> vi2;
 
-    a = solve_a(p);
-    b = solve_b(p);
+    for (const std::vector<std::string>& vs : p.tokens) {
+        vi1.push_back(std::stoi(vs[0]));
+        vi2.push_back(std::stoi(vs[1]));
+    }
+    std::sort(vi1.begin(), vi1.end());
+    std::sort(vi2.begin(), vi2.end());
+
+    a = solve_a(vi1, vi2);
+    b = solve_b(vi1, vi2);
 
     std::cout << "a: " << a << "\n";
     std::cout << "b: " << b << "\n";
