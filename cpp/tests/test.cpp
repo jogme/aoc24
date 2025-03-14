c68f2d46c29a924d16a57ee9f4ec023b0e713e6c
Norbert Pocs <norbertpocs0@gmail.com>
2025-03-14 16:38:56 +0100

    cpp: Add basic tests for parser
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

--- cpp/tests/test.cpp
+++ cpp/tests/test.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+/* tests for utility functions */
 
-int main() {
-    std::cout << "woah\n";
-    return 0;
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+// turn off for detailed debugging
+#define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
+#include "doctest.h"
+
+#include <unistd.h>
+#include <cstdio>
+
+#define READ_FILE_TEST_NAME "build/tests/read_test"
+#define READ_FILE_TEST_CONTENT "Ay-Ay-Ay Your soul's too dry,"
+
+#define WRITE_FILE_TEST_NAME "build/tests/write_test"
+#define WRITE_FILE_TEST_CONTENT "Rick and Morty in the parallel universe"
+
+#include "Downloader.h"
+
+TEST_SUITE_BEGIN("downloader" * doctest::description("Tests the class Downloader"));
+
+TEST_CASE("read_file function") {
+    Downloader d(1);
+    std::vector<std::string> vs;
+
+    CHECK_THROWS(d.read_file("non_existento_filo"));
+    vs = d.read_file(READ_FILE_TEST_NAME);
+    CHECK(vs[0] == READ_FILE_TEST_CONTENT);
 }
+
+TEST_CASE("write_file function") {
+    Downloader d(1);
+    std::vector<std::string> vs = {WRITE_FILE_TEST_CONTENT};
+
+    d.write_file(WRITE_FILE_TEST_NAME, vs);
+    CHECK_FALSE(access(WRITE_FILE_TEST_NAME, F_OK) == -1);
+
+    vs.clear();
+    vs = d.read_file(WRITE_FILE_TEST_NAME);
+    CHECK(vs[0] == WRITE_FILE_TEST_CONTENT);
+
+    std::remove(WRITE_FILE_TEST_NAME);
+}
+
+TEST_SUITE_END();
