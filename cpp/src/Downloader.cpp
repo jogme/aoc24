e69ecfe894ecc7ad89b62575111f902a7f6a24e9
Norbert Pocs <norbertpocs0@gmail.com>
2025-03-16 19:23:39 +0100

    cpp: Fix getting the input from the webpage
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

--- cpp/src/Downloader.cpp
+++ cpp/src/Downloader.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include <cstring>
 
 #include "Downloader.h"
 
@@ -21,21 +22,35 @@ Downloader::Downloader(int day) : day{day} {
     this->token = tmp[0];
 }
 
+static size_t callback(char *data, size_t size, size_t nmemb, void **userp) {
+    size_t total_size = nmemb*size;
+    static size_t current_size = 0;
+
+    *userp = realloc(*userp, current_size+total_size+1);
+    memset(*(char**)userp+current_size, 0, total_size+1);
+    memcpy(*(char**)userp+current_size, data, total_size);
+    current_size += total_size;
+
+    return total_size;
+}
+
 std::string Downloader::get_input_data() {
-    CURL *handle = NULL;
+    CURL *handle;
+    CURLcode result;
+    char* response_data = NULL;
+
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
     handle = curl_easy_init();
+    if (!handle) {
+        throw std::runtime_error("Could not initialize curl handle\n");
+    }
     curl_easy_setopt(handle, CURLOPT_URL, this->url.c_str());
-    curl_easy_setopt(handle, CURLOPT_XOAUTH2_BEARER, this->token.c_str());
-    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION,
-                     [](char *data, size_t size, size_t nmemb, void *userp) {
-        *static_cast<std::ostream*>(userp) << data;
-        return size * nmemb;
-    });
-    std::ostringstream response;
-    curl_easy_setopt(handle, CURLOPT_WRITEDATA, &response);
-    CURLcode result = curl_easy_perform(handle);
+    curl_easy_setopt(handle, CURLOPT_COOKIE, this->token.c_str());
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, callback);
+    curl_easy_setopt(handle, CURLOPT_WRITEDATA, &response_data);
+
+    result = curl_easy_perform(handle);
 
     if (result != CURLE_OK) {
         curl_easy_cleanup(handle);
@@ -46,7 +61,10 @@ std::string Downloader::get_input_data() {
     curl_easy_cleanup(handle);
     curl_global_cleanup();
 
-    return response.str();
+    std::string s(response_data);
+    free(response_data);
+
+    return s;
 }
 
 /* The token is awaited to be on a single line */
