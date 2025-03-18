2c3ba7a3d73bcb2715f58f52c173bcb522e7b640
Norbert Pocs <norbertpocs0@gmail.com>
2025-03-18 17:45:52 +0100

    cpp: Add program options to select days
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

--- cpp/src/aoc.cpp
+++ cpp/src/aoc.cpp
@@ -2,12 +2,31 @@
  * @author: Norbert Pocs
  */
 
+#include <unistd.h>
+#include <iostream>
+
 #include "Parser.h"
 #include "aoc.h"
 
-int main() {
-    Parser p(1, false);
+int main(int argc, char **argv) {
+    int opt, day;
+    bool dummy = false;
 
+    while((opt = getopt(argc, argv, "d")) != -1) {
+        switch (opt) {
+            case 'd': dummy = true;
+        }
+    }
+
+    if (optind >= argc) {
+        std::cerr << "Please provide the requested day as an argument\n";
+        return -1;
+    }
+
+    day = std::stoi(argv[optind]);
+
+    Parser p(day, dummy);
     solve_one(p);
+
     return 0;
 }
