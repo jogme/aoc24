ee1b8f832e19d587d9e982ec690fc24a067d4a69
Norbert Pocs <norbertpocs0@gmail.com>
2025-03-21 17:07:32 +0100

    cpp: Solve day 2 part b
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

--- cpp/src/two.cpp
+++ cpp/src/two.cpp
@@ -9,19 +9,41 @@
 
 using std::vector;
 
+static bool solve_two_sub(vector<int>& v) {
+    bool desc = v[1] < v[0];
+    for (size_t i=1; i<v.size(); i++) {
+        if (v[i] == v[i-1] || ((v[i] >= v[i-1]) ^ !desc) || (abs(v[i]-v[i-1]) > 3)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 static int solve_two_a(vector<vector<int>>& in) {
     int safe = 0;
     for (auto& v : in) {
-        bool desc = v[1] < v[0];
-        bool good = true;
-        for (size_t i=1; i<v.size(); i++) {
-            if (v[i] == v[i-1] || ((v[i] >= v[i-1]) ^ !desc) || (abs(v[i]-v[i-1]) > 3)) {
-                good = false;
-                break;
-            }
-        }
-        if (good) {
+        if (solve_two_sub(v)) {
             safe++;
+        }
+    }
+
+    return safe;
+}
+
+static int solve_two_b(vector<vector<int>>& in) {
+    int safe = 0;
+    for (auto& v : in) {
+        if (solve_two_sub(v)) {
+            safe++;
+        } else {
+            for (size_t i=0; i<v.size(); i++) {
+                vector<int> sub = v;
+                sub.erase(sub.begin() + i);
+                if (solve_two_sub(sub)) {
+                    safe++;
+                    break;
+                }
+            }
         }
     }
 
@@ -32,5 +54,5 @@ void solve_two(Parser& p) {
     p.tokenize_data_int();
 
     std::cout << "a: " << solve_two_a(p.tokens_int) << "\n";
-    //std::cout << "b: " << solve(p.tokens_int, true) << "\n";
+    std::cout << "b: " << solve_two_b(p.tokens_int) << "\n";
 }
